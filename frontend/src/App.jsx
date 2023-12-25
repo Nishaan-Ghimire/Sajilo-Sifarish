64119b776ac5fd97e55176004fee196c9f8ce7d6
me-nk007 <nishantkarn007@gmail.com>
2023-12-25 20:11:52 +0545

    tailwind setup

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,14 +1,9 @@
-import './App.css'
+import "./App.css";
 
 function App() {
-
-  return (
-    <>
-<h1>Hi there
-  
-</h1>
-    </>
-  )
+  return <>
+  <div className="mx-7">Nishant</div>
+  </>;
 }
 
-export default App
+export default App;
