4df3153c175d6b2381d40905d7efdd78f3c4e65e
Nishaan-Ghimire <nishanghimire50@gmail.com>
2023-12-26 09:57:31 +0545

    status added

--- backend/src/models/sifarish.model.js
+++ backend/src/models/sifarish.model.js
@@ -2,19 +2,6 @@ import mongoose from 'mongoose';
 
 
 
-// const SifarishtypeSchema = new mongoose.Schema({
-//     sifarish_id:{type: String, required: true},
-//     title: { type: String, required: true },
-//     type: { type: String, enum: ['form', 'file'], required: true },
-//     name: { type: String, required: true },
-//     label: { type: String, required: true },
-//     required: { type: Boolean, default: true },
-    
-//   });
-
-// const SifarishtypesModel = mongoose.model('SIFARISH_TYPES', SifarishtypeSchema);
-
-
 const SifarishValueSchema = new mongoose.Schema({
     sifarish_id:{type: String, required: true},
     title: { type: String, required: true },
@@ -24,6 +11,7 @@ const SifarishValueSchema = new mongoose.Schema({
     address: { type: String, required: true },
     applicantName: {type: String, required: true},
     applicantMobileNumber: { type: String, default: true },
+    status: {type: String,enum:["Pending","Success","Failed"], default: "Pending"},
     user_id: {type: String, default: true}
     
   });
