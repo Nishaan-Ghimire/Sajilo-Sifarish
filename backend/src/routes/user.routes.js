38dba885f9bae095551b2c42ee5e758313a4399b
nabind47 <nabindhami619@gmail.com>
2023-12-26 12:31:54 +0545

    chat/imageupload

--- backend/src/routes/user.routes.js
+++ backend/src/routes/user.routes.js
@@ -19,8 +19,28 @@ router.post("/login", loginUser);
 // protected routes
 router.post("/logout", authMiddleware, logoutUser);
 router.get("/profile", authMiddleware, getProfile);
+
 router
   .route("/upload")
   .patch(authMiddleware, multerMiddleware.single("image"), uploadImage);
+
+router.route("/check").patch(
+  multerMiddleware.fields([
+    { name: "front", maxCount: 1 },
+    { name: "back", maxCount: 1 },
+  ]),
+  (req, res) => {
+    const frontPath = req?.files?.front[0]?.path;
+    const backPath = req?.files?.back[0]?.path;
+
+    if (!frontPath || !backPath) {
+      return res.status(400).json({ message: "Images are required!" });
+    }
+
+    // Process the images as needed
+
+    return res.status(200).json({ message: "Images uploaded successfully" });
+  }
+);
 
 export default router;
